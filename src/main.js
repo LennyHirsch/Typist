6c4d78e962804d68e690a862699cc06ff08c09ed
LennyHirsch <lenny.hirsch@gmail.com>
2023-09-28 23:39:32 +0100

    Trying to get file dialogs working.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 const { invoke } = window.__TAURI__.tauri;
+// import { open } from '@tauri-apps/api/dialog';
 
 // let greetInputEl;
 // let greetMsgEl;
 let textarea;
 let button;
 let file = "test.txt";
+// let name;
 //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 //   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+// }
+// async function openDialog() {
+//     const selected = await open({
+//       multiple: false
+//     });
+//     console.log(selected);
+//     return selected;
 // }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -19,9 +28,14 @@ window.addEventListener("DOMContentLoaded", () => {
     textarea.value = message;
   });
   document.getElementById("title").innerHTML = file;
-
   document.querySelector("#save").addEventListener("click", (e) => {
     e.preventDefault();
     invoke('save_content', {filename: "test.txt", content: textarea.value});
-  })
+  });
+
+  // document.querySelector("#open").addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   name = openDialog();
+  //   document.getElementById("title").innerHTML = name;
+  // });
 });
